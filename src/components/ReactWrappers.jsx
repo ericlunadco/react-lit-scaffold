3233f81646853f53d1d53feccbbc01cee6d50719
Eric Luna <32916033+ericlunadev@users.noreply.github.com>
2025-02-07 17:37:09 -0300

    Fix wrong import

--- src/components/ReactWrappers.jsx
+++ src/components/ReactWrappers.jsx
@@ -5,7 +5,7 @@ import { Button } from '../conduit-components/src/components/Button/conduit-butt
 import { PieChart } from '../conduit-components/src/components/PieChart/pie-chart';
 import { Modal } from '../conduit-components/src/components/Modal/usa-modal';
 import { ConduitTable } from '../conduit-components/src/components/Table/table';
-import { Accordion } from '../conduit-components/src/components/Accordion2/conduit-accordion';
+import { Accordion } from '../conduit-components/src/components/Accordion/conduit-accordion';
 import { Banner } from '../conduit-components/src/components/Banner/conduit-banner';
 import { Breadcrumb } from '../conduit-components/src/components/Breadcrumb/conduit-breadcrumb';
 import { ButtonGroup } from '../conduit-components/src/components/ButtonGroup/conduit-button-group';
